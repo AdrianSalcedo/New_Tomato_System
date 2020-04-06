1eef4bcf42af78de1ffc9e09244735b76209a8cf
Gabriel Adrián Salcedo Varela <51538713+AdrianSalcedo@users.noreply.github.com>
2020-04-06 16:48:47 -0600

    adjoint, cost, all state variable ploted

--- New_Tomato_SystemPy.py
+++ New_Tomato_SystemPy.py
@@ -1,5 +1,6 @@
 from forward_backward_sweep import ForwardBackwardSweep
 from matplotlib import rcParams
+from scipy.integrate import odeint
 import pandas as pd
 
 # rcParams['font.family'] = 'sans-serif'
@@ -26,9 +27,9 @@ import numpy as np
 beta_y_p = 0.5
 r_y_1 = 0.004
 r_y_2 = 0.003
-r_a = 0.003
-alpha = 0.2
-beta_a_p = 0.5
+r_a = 0.017
+alpha = 0.02
+beta_a_p = 0.1
 b_y = 0.025
 b_a = 0.050
 beta_y_v = 0.00015
@@ -39,8 +40,8 @@ mu = 0.3
 #
 #
 # Initial conditions
-s_y_p_zero = 0.7
-s_a_p_zero = 0.3
+s_y_p_zero = 1.0
+s_a_p_zero = 0.0
 l_y_p_zero = 0.0
 l_a_p_zero = 0.0
 i_y_p_zero = 0.0
@@ -68,27 +69,67 @@ fbsm.set_parameters(beta_y_p, r_y_1, r_y_2, r_a, alpha, beta_a_p, b_y,
                        s_y_p_zero, s_a_p_zero, l_y_p_zero, l_a_p_zero, i_y_p_zero, i_a_p_zero, s_v_zero, i_v_zero)
 
 t = fbsm.t
-x_wc = fbsm.runge_kutta_forward(fbsm.u)
+
+x_wc_1 = fbsm.runge_kutta_forward(fbsm.u)
 #
 [x, lambda_, u] = fbsm.forward_backward_sweep()
+cost = fbsm.control_cost(fbsm.x,u)
+
+#plt.plot(t,cost,'k')
+#plt.show()
+########################################################################################################################
+########################################## R_0 Computation #############################################################
+########################################################################################################################
+
+R_0 = np.sqrt(beta_y_p/gamma)*np.sqrt((beta_y_v * mu * b_y)/(gamma * (b_y * r_y_2 + r_y_1 * r_y_2)))
+
+print(R_0)
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+#def rhs(y, t_zero):
+#    s_j_p = y[0]
+#    s_a_p = y[1]
+#   l_j_p = y[2]
+#    l_a_p = y[3]
+#    i_j_p = y[4]
+#    i_a_p = y[5]
+#    s_v = y[6]
+#    i_v = y[7]
+
+#    s_j_p_prime = - beta_y_p * s_j_p * i_v + r_y_1 * l_j_p + r_y_2 * i_j_p+ r_a * i_a_p - alpha * s_j_p
+#    s_a_p_prime = - beta_a_p * s_a_p * i_v + alpha * s_j_p
+#    l_j_p_prime = beta_y_p * s_j_p * i_v - b_y * l_j_p - r_y_1 * l_j_p
+#    l_a_p_prime = beta_a_p * s_a_p * i_v - b_a * l_a_p
+#    i_j_p_prime = b_y * l_j_p - r_y_2 * i_j_p
+#    i_a_p_prime = b_a * l_a_p - r_a * i_a_p
+#    s_v_prime = - beta_y_v * s_v * i_j_p - beta_a_v * s_v * i_a_p - gamma * s_v + (1-theta) *  mu
+#    i_v_prime = beta_y_v * s_v * i_j_p + beta_a_v * s_v * i_a_p - gamma * i_v + theta * mu
+#    rhs_np_array = np.array([s_j_p_prime, s_a_p_prime, l_j_p_prime, l_a_p_prime, i_j_p_prime, i_a_p_prime, s_v_prime, i_v_prime])
+#    return (rhs_np_array)
+#y_zero = np.array([1.0, 0.0, 0.0, 0.0, 0.0 , 0.0, 0.92, 0.08])
+#sol = odeint(rhs, y_zero, t)
+
+########################################################################################################################
 
 mpl.style.use('ggplot')
 # plt.ion()
 # n_whole = fbsm.n_whole
 ax1 = plt.subplot2grid((4, 2), (0, 0), rowspan=4)
-ax1.plot(t, x_wc[:, 4],
-         label="Infected young without control",
-         color='black'
-         )
-ax1.plot(t, x_wc[:, 5],
+#ax1.plot(t, x_wc_1[:, 4],
+#         label="Infected adult without control",
+#         color='gray'
+#         )
+ax1.plot(t, x_wc_1[:, 5],
          label="Infected adult without control",
          color='darkgreen'
          )
-ax1.plot(t, x[:, 4],
-         label="Optimal controlled",
-         color='red')
+#ax1.plot(t, x[:, 4],
+#         label="Optimal controlled adult",
+#         color='red'
+#         )
 ax1.plot(t, x[:, 5],
-         label="Optimal controlled",
+         label="Optimal controlled adult",
          color='orange')
 ax1.set_ylabel(r'Infected plants ratio')
 ax1.set_xlabel(r'Time (days)')
@@ -96,7 +137,7 @@ ax1.legend(loc=0)
 ax2 = plt.subplot2grid((4, 2), (0, 1))
 ax2.plot(t, u[:, 0],
          label="$u_1(t)$ : Remove latent young plants",
-         color='orange')
+         color='darkgreen')
 ax2.set_ylabel(r'$u_1(t)$')
 ax2.set_xlabel(r'Time(days)')
 ax3 = plt.subplot2grid((4, 2), (1, 1))
@@ -125,7 +166,35 @@ fig.savefig(name_file_1,
             # additional_artists=art,
             bbox_inches="tight")
 #######################################################################################################################
-#plt.figure()
+plt.figure()
+plt.plot(t,cost,'k')
+plt.xlabel(r'Time(days)')
+plt.ylabel(r'Cost')
+
+plt.figure()
+plt.plot(t,x[:,0], label="young susceptible")
+plt.plot(t,x[:,1], label="adult susceptible")
+plt.plot(t,x[:,2], label="young latent")
+plt.plot(t,x[:,3], label="adult latent")
+plt.plot(t,x[:,4], label="young infected")
+plt.plot(t,x[:,5], label="adult infected")
+plt.xlabel(r'Time(days)')
+plt.ylabel(r'Proportion of plant')
+plt.legend(loc=0)
+########################################################################################################################
+plt.figure()
+plt.plot(t,lambda_[:,0], label="$\lambda_1$")
+plt.plot(t,lambda_[:,1], label="$\lambda_2$")
+plt.plot(t,lambda_[:,2], label="$\lambda_3$")
+plt.plot(t,lambda_[:,3], label="$\lambda_4$")
+plt.plot(t,lambda_[:,4], label="$\lambda_5$")
+plt.plot(t,lambda_[:,5], label="$\lambda_6$")
+plt.plot(t,lambda_[:,6], label="$\lambda_7$")
+plt.plot(t,lambda_[:,7], label="$\lambda_8$")
+plt.xlabel(r'Time(days)')
+plt.ylabel(r'Proportion of adjoint variable')
+plt.legend(loc=0)
+
 #Cost_value = (A_1 * x[:, 2] + A_2 * x[:, 1]+ A_3 * x[:, 4]+ c_1 * u[:, 0] ** 2 + c_2 * u[:, 1] ** 2) * (365 / 10000)
 
 #Int_Cost_value = np.cumsum(Cost_value)
